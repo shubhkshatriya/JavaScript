f729ab2178101d3a087953b68b0fb2a229851022
Shubham Pahilwan <37569960+shubhkshatriya@users.noreply.github.com>
2024-04-20 05:05:33 +0000

    Comparisions

---
+++ 01_Basics/04_comaprision.js
@@ -0,0 +1,22 @@
+// console.log( 2 > 1);
+// console.log( 2 >= 1);
+// console.log( 2 < 1);
+// console.log( 2 == 1);
+// console.log( 2 != 1);
+
+
+console.log( "2" === 2);
+ console.log( "02" > 1);
+
+
+
+// console.log(null > 0);  // false
+// console.log(null == 0); // false
+// console.log(null >= 0); // true   javascript >= converts null to 0
+
+
+
+//*****    === strict check. this check values along with its data types
+
+
+
