31cd9ce5ad56ef62ec0890df6a444e06c72f6626
Shubham Pahilwan <37569960+shubhkshatriya@users.noreply.github.com>
2024-04-21 15:09:41 +0000

    truthy

---
+++ 04_ControlFlow/02_truthy.js
@@ -0,0 +1,16 @@
+// falsy values
+    //false, 0, -0, BigInt 0n, "", null,undefind, NaN other than these values all are truthy values
+
+// truthy values
+    // "0", 'false', " ", [], {}, function(){}, 
+
+
+// let emptyObj = {
+
+// }
+
+// if(Object.keys(emptyObj).length === 0){
+//     console.log("Object is empty");
+// }
+
+// Nullish coalescing operator (??) : we can check null undefined
